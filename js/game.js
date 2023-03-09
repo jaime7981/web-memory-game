483830774f3a86b016681cf56f40fb0224132a30
jaime7981 <jmfernandez1@miaundes.cl>
2023-03-09 13:32:09 -0300

    layout, colors and spacing first milestone

--- js/game.js
+++ js/game.js
@@ -1,2 +1,21 @@
 
+function loadCards() {
+    let mainGameClass = document.getElementsByClassName('main-game-section')[0];
+    let numberOfCards = document.getElementById('ddl-size');
+    console.log(numberOfCards.value);
+    let cardNumbers = numberOfCards.value;
 
+    mainGameClass.innerHTML = null;
+    for (let i = 0; i < cardNumbers; i++) {
+        let newCard = document.createElement("div");
+        newCard.className = "card";
+        newCard.innerHTML = (i + 1).toString();
+        mainGameClass.appendChild(newCard);
+    }
+}
+
+window.onload = function pageonLoad() {
+    loadCards();
+    let numberOfCards = document.getElementById('ddl-size');
+    numberOfCards.onchange = loadCards;
+}
