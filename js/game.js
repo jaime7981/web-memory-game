bf617346735001a4a7ec6679fe7ecd9d2ab6e90b
jaime7981 <jmfernandez1@miaundes.cl>
2023-03-10 19:39:59 -0300

    load svg into cards

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
 
 var scoreboardCompParam = "Time";
+var testSvg = "M228 319.5H12C5.64873 319.5 0.5 314.351 0.5 308V12C0.5 5.64873 5.64873 0.5 12 0.5H159.175C159.571 0.5 159.952 0.656748 160.233 0.936021L239.057 79.2681C239.341 79.5497 239.5 79.9326 239.5 80.3321V308C239.5 314.351 234.351 319.5 228 319.5Z"
+var svgHeart = "M 10,30 A 20,20 0,0,1 50,30 A 20,20 0,0,1 90,30 Q 90,60 50,90 Q 10,60 10,30 z";
 
 var fakedData = {
     "scoreboard":[
@@ -36,13 +38,57 @@ function loadCards() {
     let numberOfCards = document.getElementById('ddl-size');
     let cardNumbers = numberOfCards.value;
 
+    let loadFirstCardsCounter = 0;
+
     mainGameClass.innerHTML = null;
     for (let i = 0; i < cardNumbers; i++) {
         let newCard = document.createElement("div");
         newCard.className = "card";
-        newCard.innerHTML = (i + 1).toString();
+        // newCard.innerHTML = (i + 1).toString();
+        // newCard.appendChild(insertSvgIntoCard());
+
+        if (loadFirstCardsCounter < 4 & loadFirstCardsCounter > 1) {
+            newCard.appendChild(insertSvgIntoCard());
+        }
+        loadFirstCardsCounter += 1;
+
         mainGameClass.appendChild(newCard);
     }
+}
+
+function insertSvgIntoCard() {
+    let svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    let svgPath = document.createElementNS('http://www.w3.org/2000/svg',"path");
+    svgElement.setAttribute("viewBox", "0 0 100 100");
+    svgElement.appendChild(createRandomPath(svgPath));
+    return svgElement;
+}
+
+function createRandomPath(svgPathElement) {
+    svgPathElement.style.stroke = "black";
+    svgPathElement.style.strokeWidth = "1px";
+    //svgPathElement.setAttribute("d", svgHeart);
+    svgPathElement.setAttribute("d", generateRandomSvg());
+    return svgPathElement;
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(min + Math.random() * (min + max));
+}
+
+function generateRandomSvg() {
+    let cardHeight = 55;
+    let cardWidth = 80;
+    // let svgString = "M " + getRandomInt(cardHeight) + "," + getRandomInt(cardWidth) + " ";
+    let svgString = "M " + getRandomInt(-cardWidth/2, cardWidth/2) + "," + getRandomInt(-cardHeight/2, cardHeight/2) + " ";
+    let numberOfVectors = getRandomInt(15, 40);
+
+    for (let i = 0; i < numberOfVectors; i++) {
+        svgString += "L" + getRandomInt(30, 150) + " " + getRandomInt(30, 150) + " ";
+    }
+
+    svgString += "z";
+    return svgString;
 }
 
 function loadScoreboard(comparitionParam) {
